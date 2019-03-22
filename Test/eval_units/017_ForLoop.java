e02dbf46454cf74b736aeca0e8398a6cc4828185
qwoinet <quentin.woinet@telecom-bretagne.eu>
2019-03-23 00:53:24 +0100

    Added a test to make sure the For loop can be written in the usual way

--- Test/eval_units/017_ForLoop.java
+++ Test/eval_units/017_ForLoop.java
@@ -1,8 +1,16 @@
 //: 0
 //: 1
 //: 2
+
 //: 2
 //: 4
+
+//: 0
+//: 1
+//: 2
+//: 3
+//: 4
+
 class Main {
   static void main(){
     for(int i = 0 ; i < 3 ; i = i + 1){
@@ -11,6 +19,9 @@ class Main {
     int j;
     for(j = 2 ; j < 5 ; j = j + 2){
       Debug.debug(j);
+    }
+    for(int k = 0 ; k < 5 ; k ++){
+      Debug.debug(k);
     }
   }
 }
