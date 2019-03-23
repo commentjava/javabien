07c4b415cd71e4eaef23871dbf9a808fbc5b974b
qwoinet <quentin.woinet@telecom-bretagne.eu>
2019-03-23 03:45:20 +0100

    Added some tests for the operators on Float values

--- Test/eval_units/019_DoubleObjects.java
+++ Test/eval_units/019_DoubleObjects.java
@@ -2,10 +2,28 @@
 //: -1022
 //: 64
 
+//: 4.68
+//: 5.68
+//: -4.68
+
 //: 6.68
 //: 9.36
 //: 2.34
 //: -2.68
+
+//: 5.68
+//: 4.68
+//: 13.68
+//: 4.68
+//: 42.12
+//: 4.68
+
+//: false
+//: true
+//: false
+//: true
+//: false
+//: true
 
 class Main{
   static void main(){
@@ -15,11 +33,29 @@ class Main{
     Debug.debug(Double.MIN_EXPONENT);
     Debug.debug(Double.SIZE);
 
-    double b = 2.0, c = 4.68, d = 1;
+    double b = 2.0, c = 4.68, d = 9;
+    Debug.debug(c++);
+    Debug.debug(c--);
+    Debug.debug(-c);
+
     Debug.debug(b + c);
     Debug.debug(b * c);
     Debug.debug(c / b);
     Debug.debug(b - c);
+
+    Debug.debug(++c);
+    Debug.debug(--c);
+    Debug.debug(c += d);
+    Debug.debug(c -= d);
+    Debug.debug(c *= d);
+    Debug.debug(c /= d);
+
+    Debug.debug(b == c);
+    Debug.debug(b != c);
+    Debug.debug(b > c);
+    Debug.debug(b < c);
+    Debug.debug(b >= c);
+    Debug.debug(b <= c);
 
     Debug.dumpMemory();
   }
